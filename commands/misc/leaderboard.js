d1ff53deef1259ebb294367a4f429a355ed139f0
Glitch (grandiose-crocus-geography) <none>
2021-03-22 18:03:39 +0000

    📫🏓 Checkpoint ./commands/misc/leaderboard.js:26780611/113

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -4,34 +4,35 @@ const db = require("quick.db");
 const fs = require("fs");
 module.exports = {
   name: "leaderboard",
+  category: "j",
   run: async (client, message, args) => {
     let data = db
       .all()
       .filter(i => i.ID.startsWith(`xp_`))
       .sort((a, b) => b.data - a.data);
     if (data.length < 1) return message.channel.send("No leaderboard");
+    let llb = [];
     let myrank =
       data
         .map(m => m.ID)
         .indexOf(`xp_${message.author.id}_${message.guild.id}`) + 1 || "N/A";
-    data.length = 20;
-    let lb = [];
+    data.length = 7;
     for (let i in data) {
       let id = data[i].ID.split(`_`)[1];
-      let user = await client.users.fetch(id);
+      let user = await client.users.get(id);
       user = user ? user.tag : "Unknown User#0000";
-      const check = lb.find(x => x.id === message.author.id);
+      const check = llb.find(x => x.id === message.author.tag);
       let rank = data.indexOf(data[i]) + 1;
       let level = db.get(`level_${id}_${message.guild.id}`) || 0;
       let xp = data[i].data;
       if (check) {
-        let numb = lb.indexOf(check);
-        lb[numb].rank.push(rank);
-        lb[numb].xp.push(xp);
-        lb[numb].level.push(level);
+        let numb = llb.indexOf(check);
+        llb[numb].rank(rank);
+        llb[numb].xp(xp);
+        llb[numb].level(level);
       } else {
-        lb.push({
-          user: {id, tag: user },
+        llb.push({
+          user: { id, tag: user },
           rank: rank,
           level: level,
           xp: xp
@@ -39,7 +40,7 @@ module.exports = {
       }
     }
     const embed = new MessageEmbed().setTitle("Leaderboard").setColor("RANDOM");
-    lb.forEach(d => {
+    llb.forEach(d => {
       embed.addField(
         d.rank,
         `${d.user.tag}\n exp: ${d.xp}\n level: ${d.level}`
