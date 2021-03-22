fc3c5c5760c76f4f89d76d1f425a4022d826dd03
Glitch (grandiose-crocus-geography) <none>
2021-03-22 18:14:54 +0000

    🌍🏚 Checkpoint ./commands/misc/leaderboard.js:26780611/7614

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -6,47 +6,41 @@ module.exports = {
   name: "leaderboard",
   category: "j",
   run: async (client, message, args) => {
-    let data = db
+    const coins = db
       .all()
-      .filter(i => i.ID.startsWith(`xp_`))
+      .filter(data => data.ID.startsWith(`level`))
       .sort((a, b) => b.data - a.data);
-    if (data.length < 1) return message.channel.send("No leaderboard");
-    let llb = [];
-    let myrank =
-      data
-        .map(m => m.ID)
-        .indexOf(`xp_${message.author.id}_${message.guild.id}`) + 1 || "N/A";
-    data.length = 7;
-    for (let i in data) {
-      let id = data[i].ID.split(`_`)[1];
-      let user = await client.users.get(id);
-      user = user ? user.tag : "Unknown User#0000";
-      const check = llb.find(x => x.id === message.author.tag);
-      let rank = data.indexOf(data[i]) + 1;
-      let level = db.get(`level_${id}_${message.guild.id}`) || 0;
-      let xp = data[i].data;
-      if (check) {
-        let numb = llb.indexOf(check);
-        llb[numb].rank(rank);
-        llb[numb].xp(xp);
-        llb[numb].level(level);
-      } else {
-        llb.push({
-          user: { id, tag: user },
-          rank: rank,
-          level: level,
-          xp: xp
-        });
-      }
+    const userBalance = await db.fetch(
+      `level_${message.author.id}_${message.guild.id}`
+    );
+
+    coins.length = 10;
+    let finalLb = "";
+
+    for (let i in coins) {
+      if (coins[i].data === null) coins[i].data = 0;
+
+      let userData = client.users.cache.get(coins[i].ID.split("_")[1])
+        ? client.users.cache.get(coins[i].ID.split("_")[1]).tag
+        : "Unknown#0000";
+
+      finalLb += `__**${coins.indexOf(coins[i]) + 1}.**__ **${userData} » \`${
+        coins[i].data
+      }\`**\n`;
     }
-    const embed = new MessageEmbed().setTitle("Leaderboard").setColor("RANDOM");
-    llb.forEach(d => {
-      embed.addField(
-        d.rank,
-        `${d.user.tag}\n exp: ${d.xp}\n level: ${d.level}`
+
+    let embed = new MessageEmbed()
+      .setTitle(`**Level Leaderboard 💰**`)
+      .setDescription(
+        `
+            ${finalLb}
+            `
+      )
+      .setColor("#efcb83")
+      .setFooter(
+        `Your Level » ${userBalance} | Leaderboards are Global Statistics`
       );
-    });
-    embed.setFooter(`Your Position: ${myrank}`);
-    return message.channel.send(embed);
+
+    message.channel.send(embed);
   }
 };
