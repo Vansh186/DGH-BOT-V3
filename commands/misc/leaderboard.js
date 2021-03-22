841f524cda6a390db7c0e6129d8651d6d870ca35
Glitch (grandiose-crocus-geography) <none>
2021-03-22 04:39:41 +0000

    💴🌨 Checkpoint ./commands/misc/leaderboard.js:26780611/343

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -4,16 +4,20 @@ const Discord = require("discord.js");
 module.exports = {
   name: "leaderboard",
   description: "Check the sever's leaderboard",
-  category: "misc",
+  category: "mis",
   botpermission: ["MANAGE_GUILD"],
 
   async run(client, message, args) {
-    let xp = db.get(`xp_${message.member.id}_${message.guild.id}`, { sort: ".data" });
+    let xp = db.startswith(`xp_${message.member.id}_${message.guild.id}`, { sort: ".data" });
 
-    let content = "";
+let content = "";
 
-      content += `${0 + 1}. ${xp} - ${xp.data} \n`;
+        for (let i = 0; i < xp.length; i++){
+            let user = client.users.cache.get(xp[i].ID.split('_')[1]).username
 
+            content += `${i+1}. ${user} - ${xp[i].data} \n`;
+
+         
       const embed = new Discord.MessageEmbed()
         .setTitle(`${message.guild.name}'s Leaderboard`)
         .setDescription(`${content}`)
@@ -23,4 +27,4 @@ module.exports = {
       message.channel.send(embed);
     }
   
-};
+}};
