5d0136f52ff9bc7e0d56d84176ef9b1ba9f7075b
Glitch (grandiose-crocus-geography) <none>
2021-03-22 14:48:35 +0000

    🐠👦 Checkpoint ./commands/misc/leaderboard.js:26780611/5765

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const db = require ("quick.db")
+const db = require("quick.db");
 const { getInfo } = require("../../level-xp/xp.js");
 
 module.exports = {
@@ -11,27 +11,30 @@ module.exports = {
       .sort((a, b) => b.data - a.data);
     if (data.length < 1) return message.channel.send("No leaderboard");
     let myrank =
-      data.map(m => m.ID).indexOf(`xp_${message.author.id}_${message.guild.id}`) + 1 || "N/A";
+      data
+        .map(m => m.ID)
+        .indexOf(`xp_${message.author.id}_${message.guild.id}`) + 1 || "N/A";
     data.length = 20;
     let lb = [];
     for (let i in data) {
-      let id = data[i].ID.split(`_${message.guild.id}`)[1];
+      let id = data[i].ID.split(`_`)[1];
       let user = await client.users.fetch(id);
       user = user ? user.tag : "Unknown User#0000";
       let rank = data.indexOf(data[i]) + 1;
-      let level = 3//db.get(`xp_${id}`);
+      let x0p = db.get(`xp_${user.id}_${message.guild.id}`) || 0;
+     let level = getInfo(x0p);; //db.get(`xp_${id}`);
       let xp = data[i].data;
-      let xpreq = Math.floor(Math.pow(level / 0.1, 2));
       lb.push({
         user: { id, tag: user },
         rank,
         level,
-        xp,
-        xpreq
-})
-      const embed = new MessageEmbed().setTitle("Leaderboard").setColor("RANDOM");
+        xp});
+    }
+    const embed = new MessageEmbed().setTitle("Leaderboard").setColor("RANDOM");
     lb.forEach(d => {
-      embed.addField(`${d.rank}. ${d.user.tag}\n ${d.xp} exp\n level ${d.level}`);
+      embed.addField(d.rank,
+        `${d.user.tag}\n exp: ${d.xp}\n level: ${d.level}`
+      );
     });
     embed.setFooter(`Your Position: ${myrank}`);
     return message.channel.send(embed);
