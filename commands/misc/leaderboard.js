47ac856636b51b48dffa04b8c7234e71672734f3
Glitch (grandiose-crocus-geography) <none>
2021-03-22 13:40:40 +0000

    📰🍣 Checkpoint ./commands/admin/botlist.js:26780611/1515 ./commands/misc/leaderboard.js:26780611/1670

---
+++ commands/misc/leaderboard.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+const { getInfo } = require("../../level-xp/xp.js");
+
+module.exports = {
+  name: "leaderboard",
+  description: "Shows top 10 users with the highest amount of XP",
+  category: "levels",
+   run: async (client, message, args) => {
+    const embed = new MessageEmbed()
+      .setTitle(`${message.guild.name} 's Leaderboard`)
+      .setColor("BLUE")
+      .setFooter(message.author.username)
+      .setTimestamp();
+      const guildId = message.guild.id;
+      const userId = message.guild.author.id //ldata[i].ID.replace(`xp_${guildId}_`, ""); // get user id
+     let oldxp = db.get(`xp_${userId}_${guildId}`);
+       const { level, remxp, levelxp } = getInfo(oldxp);
+  const user = client.users.cache.get(userId); // Get user
+     
+      if (user) {
+        embed.addField(user.username, `${oldxp}xp`, true);
+      }
+    
+
+    message.channel.send({ embed });
+  },
+};
