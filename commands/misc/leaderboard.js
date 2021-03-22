be8be2efd12703908026c40dd359290886826758
Glitch (grandiose-crocus-geography) <none>
2021-03-22 17:04:03 +0000

    🌛👀 Checkpoint ./commands/misc/leaderboard.js:26780611/6135

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -5,9 +5,9 @@ const fs = require("fs");
 module.exports = {
   name: "leaderboard",
   run: async (client, message, args) => {
-    let data = db
+     let data = db
       .all()
-      .filter(i => i.ID.startsWith("xp_"))
+      .filter(i => i.ID.startsWith(`xp_${wtf}_${message.guild.id}`))
       .sort((a, b) => b.data - a.data);
     if (data.length < 1) return message.channel.send("No leaderboard");
     let myrank =
@@ -15,32 +15,35 @@ module.exports = {
         .map(m => m.ID)
         .indexOf(`xp_${message.author.id}_${message.guild.id}`) + 1 || "N/A";
     data.length = 20;
+    let lb = [];
     for (let i in data) {
       let id = data[i].ID.split(`_`)[1];
       let user = await client.users.fetch(id);
       user = user ? user.tag : "Unknown User#0000";
+      
+      function wrt(){
+        user 
+        }
+      
+      
       let rank = data.indexOf(data[i]) + 1;
       let level = db.get(`level_${id}_${message.guild.id}`) || 0;
       let xp = data[i].data;
-      let lb = [];
       lb.push({
         user: { id, tag: user },
         rank,
         level,
         xp
       });
-
-      const embed = new MessageEmbed()
-        .setTitle("Leaderboard")
-        .setColor("RANDOM");
-      lb.forEach(d => {
-        embed.addField(
-          d.rank,
-          `${d.user.tag}\n exp: ${d.xp}\n level: ${d.level}`
-        );
-      });
-      embed.setFooter(`Your Position: ${myrank}`);
-      return message.channel.send(embed);
     }
+    const embed = new MessageEmbed().setTitle("Leaderboard").setColor("RANDOM");
+    lb.forEach(d => {
+      embed.addField(
+        d.rank,
+        `${d.user.tag}\n exp: ${d.xp}\n level: ${d.level}`
+      );
+    });
+    embed.setFooter(`Your Position: ${myrank}`);
+    return message.channel.send(embed);
   }
 };
