af8efa3308ed51e736eb1b096b7af9fde5232e8c
Glitch (grandiose-crocus-geography) <none>
2021-03-22 16:29:00 +0000

    🙈📠 Checkpoint ./commands/misc/leaderboard.js:26780611/10640

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -15,7 +15,6 @@ module.exports = {
         .map(m => m.ID)
         .indexOf(`xp_${message.author.id}_${message.guild.id}`) + 1 || "N/A";
     data.length = 20;
-    let lb = JSON.parse(fs.readFileSync("./xp.json", "utf8"));
     for (let i in data) {
       let id = data[i].ID.split(`_`)[1];
       let user = await client.users.fetch(id);
@@ -23,25 +22,34 @@ module.exports = {
       let rank = data.indexOf(data[i]) + 1;
       let level = db.get(`level_${id}_${message.guild.id}`) || 0;
       let xp = data[i].data;
-      const lv = {
-        user: { id, tag: user },
-        rank,
-        level,
-        xp
-      };
+      let p = require("../../xp.json");
+      if (!xp[message.author.id]) {
+        p[{ id, tag: user }] = {
+          xp: 0,
+          level: 0,
+          rank: 0
+        };
+      }
+      let curxp = p[{ id, tag: user }].xp;
+      let curlvl = p[{ id, tag: user }].level;
+      let currank = p[{ id, tag: user }].rank;
+      p[{ id, tag: user }] = curxp + curlvl + currank;
 
-fs.writeFile("./xp.json", JSON.stringify(lv, null, 2), err => {
-      if (err) console.log(err);
-    });
+      fs.writeFile("./xp.json", JSON.stringify(p, null, 2), err => {
+        if (err) console.log(err);
+      });
+
+      const embed = new MessageEmbed()
+        .setTitle("Leaderboard")
+        .setColor("RANDOM");
+      p.forEach(d => {
+        embed.addField(
+          d.rank,
+          `${d.user.tag}\n exp: ${d.xp}\n level: ${d.level}`
+        );
+      });
+      embed.setFooter(`Your Position: ${myrank}`);
+      return message.channel.send(embed);
     }
-    const embed = new MessageEmbed().setTitle("Leaderboard").setColor("RANDOM");
-    lb.forEach(d => {
-      embed.addField(
-        d.rank,
-        `${d.user.tag}\n exp: ${d.xp}\n level: ${d.level}`
-      );
-    });
-    embed.setFooter(`Your Position: ${myrank}`);
-    return message.channel.send(embed);
   }
 };
