ed89c20ed1d907944cebca9406305df8a0dbd51d
Glitch (grandiose-crocus-geography) <none>
2021-03-23 03:48:08 +0000

    🎓🌁 Checkpoint ./events/inviteCreate.js:26780611/487

---
+++ events/inviteCreate.js
@@ -0,0 +1,11 @@
+const guildInvites = new Map();
+module.exports = async client => {
+  
+client.on('inviteCreate', async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+    client.guilds.cache.forEach(guild => {
+        guild.fetchInvites()
+            .then(invites => guildInvites.set(guild.id, invites))
+            .catch(err => console.log(err));
+    })})};
