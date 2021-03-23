396bead6b161d73cce749b2394bc45aefbe8e732
Glitch (grandiose-crocus-geography) <none>
2021-03-23 00:21:35 +0000

    👄🐒 Checkpoint ./config.js:26780611/108 ./commands/misc/leaderboard.js:26780611/1870

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -1,26 +1,43 @@
+const { MessageEmbed } = require("discord.js");
 const db = require("quick.db");
-const Discord = require("discord.js");
-
+//const { getInfo } = require("../../XP.json");
+const fs = require("fs");
 module.exports = {
   name: "leaderboard",
-  description: "Check the sever's leaderboard",
   category: "misc",
-  botpermission: ["MANAGE_GUILD"],
+  run: async (client, message, args) => {
+    const coins = db
+      .all()
+      .filter(data => data.ID.startsWith(`xp`))
+      .sort((a, b) => b.data - a.data);
+    const userBalance = await db.fetch(
+      `level_${message.author.id}_${message.guild.id}`
+    );
+    coins.length = 10;
+    let finalLb = "";
 
-  async run(client, message, args) {
-    let xp = db.get(`xp_${message.member.id}_${message.guild.id}`, { sort: ".data" });
+    for (let i in coins) {
+      if (coins[i].data === null) coins[i].data = 1;
 
-    let content = "";
+      let userData = client.users.cache.get(coins[i].ID.split("_")[1])
+        ? client.users.cache.get(coins[i].ID.split("_")[1]).tag
+        : "Unknown#0000";
 
-      content += `${0 + 1}. ${xp} - ${xp.data} \n`;
-
-      const embed = new Discord.MessageEmbed()
-        .setTitle(`${message.guild.name}'s Leaderboard`)
-        .setDescription(`${content}`)
-        .setColor("RANDOM")
-        .setTimestamp();
-
-      message.channel.send(embed);
+      finalLb += `__**${coins.indexOf(coins[i]) + 1}.**__ **${userData} » \`${coins[i].data}\`**\n`;
     }
-  
-};
+
+    let embed = new MessageEmbed()
+      .setTitle(`**Level Leaderboard ⬆️**`)
+      .setDescription(
+        `
+            ${finalLb}
+            `
+      ) 
+      .setColor("#efcb83")
+      .setFooter(
+        `Your Level » ${userBalance} | Leaderboards are Global Statistics`
+      );
+
+    message.channel.send(embed);
+  }
+}; 
