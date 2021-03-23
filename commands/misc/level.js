245c258babe7f48a43ff9d30ef08f163ecee1e7d
Glitch (grandiose-crocus-geography) <none>
2021-03-23 12:54:39 +0000

    🙈🏄 Checkpoint ./commands/misc/level.js:26780611/4236 ./events/Level.js:26780611/1994 ./commands/misc/leaderboard.js:26780611/4529

--- commands/misc/level.js
+++ commands/misc/level.js
@@ -1,39 +1,36 @@
-//const db = require("quick.db");
-//const { getInfo } = require("../../handlers/xp.js");
-const fs = require("fs")
-let db = JSON.parse(fs.readFileSync("./database.json", "utf8"));
-const canvacord = require("canvacord");
 const Discord = require("discord.js");
 module.exports = {
-  name: "level",
-  aliases: ["lvl", "rank"],
-  description: "Get the level of Author or Mentioned",
-  usage: "level [user]",
-  category: "info",
-  run: (client, message, args) => {
-    const user = message.mentions.users.first() || message.author;
+  name: "rank",
+  aliases: ["rk", "r", "level", "lvl", "points"],
+  description: "Returns the current rank of a member.",
+  run: async (client, message, args) => {
+    const db = require("quick.db");
 
-    if (user.id === client.user.id) {
-      //IF BOT
-      return message.channel.send(":wink: | I am on level 100");
-    }
+    let member = message.mentions.members.first() || message.author;
+    if (member.bot) return message.reply("bots aren't eligible for XP!");
 
-    if (user.bot) {
-      return message.channel.send("Bot do not have levels");
-    }
+    // Data
+    let level = await db.get(`level_${message.guild.id}_${member.id}`);
+    let exp = await db.fetch(`xp_${message.guild.id}_${member.id}`);
+    let neededXP = Math.floor(Math.pow(level / 0.1, 2));
 
-        let userInfo = db[message.author.id];
-        let member = message.mentions.members.first();
-        let embed = new Discord.MessageEmbed()
-        .setColor(0x4286f4)
-        .addField("**Level**", userInfo.level)
-        .addField("**XP**", userInfo.xp+"/99");
-        if(!member) return message.channel.sendEmbed(embed)
-        let memberInfo = db[member.id]
-        let embed2 = new Discord.RichEmbed()
-        .setColor(0x4286f4)
-        .addField("Level", memberInfo.level)
-        .addField("XP", memberInfo.xp+"/4")
-        message.channel.sendEmbed(embed2)
-    }
- }
+    // Rank
+    let every = await db.all();
+    every = every
+      .filter(i => i.ID.startsWith(`xp_${message.guild.id}_`))
+      .sort((a, b) => b.data - a.data);
+    let ranking =
+      every.map(x => x.ID).indexOf(`xp_${message.guild.id}_${member.id}`) + 1;
+
+    const points = new Discord.MessageEmbed()
+      .setAuthor(`${member.username}'s Rank`, member.displayAvatarURL())
+      .addField(`XP / Points`, exp.toString(), true)
+      .addField(`Level`, level, true)
+      .addField(`Rank`, ranking, true)
+      .setColor(member.displayHexColor)
+      .setThumbnail(message.guild.iconURL())
+      .setFooter("Cooldown: 30 seconds")
+      .setTimestamp();
+    message.channel.send(points);
+  }
+};
