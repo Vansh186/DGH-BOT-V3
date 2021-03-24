7594235bfc6202a920075dafb485b84c1c50a153
Glitch (grandiose-crocus-geography) <none>
2021-03-24 09:08:36 +0000

    🖼🤘 Checkpoint ./commands/misc/leaderboard.js:26780611/2608 ./commands/misc/level.js:26780611/15

--- commands/misc/leaderboard.js
+++ commands/misc/leaderboard.js
@@ -1,6 +1,5 @@
 const { MessageEmbed } = require("discord.js");
 const db = require("quick.db");
-//const { getInfo } = require("../../XP.json");
 const fs = require("fs");
 module.exports = {
   name: "leaderboard",
@@ -11,7 +10,7 @@ module.exports = {
       .filter(data => data.ID.startsWith(`guild_${message.guild.id}_xp_`))
       .sort((a, b) => b.data - a.data);
     const userBalance = await db.fetch(
-      `level_${message.author.id}_${message.guild.id}`
+      `guild_${message.guild.id}_level_${message.author.id}`
     );
     coins.length = 10;
     let finalLb = "";
@@ -19,18 +18,18 @@ module.exports = {
     for (let i in coins) {
       if (coins[i].data === null) coins[i].data = 1;
 
-      let userData = client.users.cache.get(coins[i].ID.split("_")[1])
-        ? client.users.cache.get(coins[i].ID.split("_")[1]).tag
+      let userData = client.users.cache.get(coins[i].ID.split("_")[3])
+        ? client.users.cache.get(coins[i].ID.split("_")[3]).tag
         : "Unknown#0000";
 
-      finalLb += `__**${coins.indexOf(coins[i]) + 1}.**__ **${userData} » \`${coins[i].data}\`**\n`;
+      finalLb += `__**${coins.indexOf(coins[i]) + 1}.**__ **${userData} » \`${
+        coins[i].data
+      }\`**\n`;
     }
 
     let embed = new MessageEmbed()
       .setTitle(`**Level Leaderboard ⬆️**`)
-      .setDescription(
-        `${finalLb}`
-      ) 
+      .setDescription(`${finalLb}`)
       .setColor("#efcb83")
       .setFooter(
         `Your Level » ${userBalance} | Leaderboards are Global Statistics`
@@ -38,4 +37,4 @@ module.exports = {
 
     message.channel.send(embed);
   }
-}; 
+};
