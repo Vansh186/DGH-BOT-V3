2f036064d8450b0363763c1c708f317f979705c7
Glitch (grandiose-crocus-geography) <none>
2021-03-24 07:46:14 +0000

    🚦🚿 Checkpoint ./commands/misc/level.js:26780611/7193

--- commands/misc/level.js
+++ commands/misc/level.js
@@ -1,42 +1,49 @@
-const Discord = require('discord.js');
-const canvacord = require('canvacord')
-const db = require('quick.db')
-
-
-
-
+const Discord = require("discord.js");
+const canvacord = require("canvacord");
+const db = require("quick.db");
 module.exports = {
-	name: 'rank',
-	description: 'Rank Card',
-    guildOnly: true,
-    usage: '+rank / +rank @mention.user',
-     run: async (client, message, args) => {
-     var user = message.mentions.users.first() || message.author
-         var level = db.get(`guild_${message.guild.id}_level_${user.id}`) || 0
-         let xp = db.get(`guild_${message.guild.id}_xp_${user.id}`) || 0
-         var xpNeeded = level * 500 + 500
-         let every = db.all().filter(i => i.ID.startsWith(`guild_${message.guild.id}_xptotal_`)).sort((a, b) => b.data - a.data)
-         var rank = every.map(x => x.ID).indexOf(`guild_${message.guild.id}_xptotal_${user.id}`) + 1
+  name: "level",
+  description: "level Card",
+  usage: "level [@user]",
+  run: async (client, message, args) => {
+    var user = message.mentions.users.first() || message.author;
+    var level = db.get(`guild_${message.guild.id}_level_${user.id}`) || 0;
+    let xp = db.get(`guild_${message.guild.id}_xp_${user.id}`) || 0;
+    var xpNeeded = level * 500 + 500;
+    let every = db
+      .all()
+      .filter(i => i.ID.startsWith(`guild_${message.guild.id}_xptotal_`))
+      .sort((a, b) => b.data - a.data);
+    var rank =
+      every
+        .map(x => x.ID)
+        .indexOf(`guild_${message.guild.id}_xptotal_${user.id}`) + 1;
+    const rak = new canvacord.Rank()
 
+      .setAvatar(user.displayAvatarURL({ format: "png" }))
+      .setCurrentXP(xp)
+      .setRequiredXP(xpNeeded)
+      .setStatus(user.presence.status)
+      .setProgressBar("#FFFFFF", "COLOR")
+      .setUsername(user.username)
+      .setDiscriminator(user.discriminator)
+      .setLevel(level)
+      .setRank(rank);
 
-         const rak = new canvacord.Rank()
-           
-           .setAvatar(user.displayAvatarURL({format: "png" }))
-           .setCurrentXP(xp.toString())
-           .setRequiredXP(xpNeeded.toString())
-           .setStatus(user.presence.status)
-           .setProgressBar("#FFFFFF", "COLOR")
-           .setUsername(user.username) 
-           .setDiscriminator(user.discriminator)
-           .setLevel(level)
-           .setRank(rank)
-           
-             
-           rak.build()
-           .then(data => {
-               const attachment = new Discord.MessageAttachment(data, "RankCard.png");
-               message.channel.send(attachment);
-           });
-     
-    }
-}
+    rak.build().then(data => {
+      const attachment = new Discord.MessageAttachment(data, "RankCard.png");
+      let embed = new Discord.MessageEmbed()
+        .setAuthor(user.username, message.guild.iconURL())
+        .setColor("#ff2050")
+        .setDescription(
+          `**LEVEL** - ${level}
+           **Rank** - ${rank}
+           **XP** - ${xp}/${xpNeeded}`
+        )
+        .setImage("attachment://welcome-image.png")
+        .attachFiles(attachment);
+
+      message.channel.send(embed);
+    });
+  }
+};
