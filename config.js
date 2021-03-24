fdd885c2598b4bff20f9902db080b0ee522ee000
Glitch (grandiose-crocus-geography) <none>
2021-03-24 09:56:35 +0000

    🐊👝 Checkpoint ./config.js:26780611/118

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.Token = "NzkwOTM3MDI0OTQxMTI5NzU5.X-H3gQ.i3Nh1aLQ0OyvIj7GnzPa0QyM0Fg"; //Bot Token - Important
+exports.Token = "NzkwOTM3MDI0OTQxMTI5NzU5.X-H3gQ.3m7D12rnwnmmoOygQt42LOGqtcc"; //Bot Token - Important
 exports.Owner = "767726828311543820"; //Bot Owner ID - Important
 exports.Developer = "740947753135243354"
 exports.Default_Prefix = "!"; //Bot Default Prefix (Examples: A!, !)- Important
