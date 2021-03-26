b9617ed0e08820d6db626a84ff9f378af20e24da
Glitch (grandiose-crocus-geography) <none>
2021-03-26 23:46:17 +0000

    🐪🕌 Checkpoint ./commands/ticket/new.js:26780611/4948 ./commands/ticket/setup.js:26780611/1237

--- commands/ticket/new.js
+++ commands/ticket/new.js
@@ -1,29 +1,54 @@
 const randomstring = require("randomstring");
-let numbers = randomstring.generate({
-    length: 5,
-    charset: 'numeric'
-  });
+const Discord = require("discord.js");
 module.exports = {
-    name : 'ticket',
-    category: 'ticket',
-    
-    run : async(client, message) => {
-        message.guild.channels.create(`Ticket-${`, {
-            type : 'text',
-            parent : '756890395392081985',
-            permissionOverwrites : [
-                {
-                    id : message.guild.id,
-                    deny : ['VIEW_CHANNEL']
-                },
-                {
-                    id : message.author.id,
-                    allow : ['VIEW_CHANNEL', 'SEND_MESSAGES', 'ADD_REACTIONS', 'ATTACH_FILES']
-                }
+  name: "ticket",
+  category: "ticket",
+
+  run: async (args, client, message) => {
+    let numbers = randomstring.generate({
+      length: 5,
+      charset: "numeric"
+    });
+    const strat = client.db.get(`ticket_${message.guild.id}`)
+    const channel = await message.guild.channels
+      .create(`Ticket-${numbers}`, {
+        type: "text",
+        parent: strat,
+        topic: `TicketID: ${message.author.id}\nSubject: ${args.join(" ")}`,
+        permissionOverwrites: [
+          {
+            id: message.guild.id,
+            deny: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
+          },
+          {
+            id: message.author.id,
+            allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
+          },
+          {
+            id: client.user.id,
+            allow: [
+              "VIEW_CHANNEL",
+              "MANAGE_CHANNELS",
+              "MANAGE_MESSAGES",
+              "SEND_MESSAGES"
             ]
-        }).then(async channel=> {
-            message.reply(`click <#${channel.id}> to view your ticket`)
-            channel.send(`${message.author}, welcome to your ticket!`)
-        })
-    }
-}
+          }
+        ]
+      })
+      .catch(e => {
+        throw e;
+      });
+    await message.channel.send(
+      new Discord.MessageEmbed()
+        .setColor(0x00ff00)
+        .setDescription(
+          `Your ticket has been created! <#${channel.id}>\nWe will contact you in the ticket shortly!`
+        )
+        .setTimestamp()
+        .setAuthor(
+          "Unicron Ticket System",
+          client.user.displayAvatarURL({ dynamic: true })
+        )
+    );
+  }
+};
---
+++ commands/ticket/setup.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "setup",
+  usage: `setup <category_id>`,
+  category: "ticket",
+  description: "Category ticket setup",
+  args: true,
+  cooldown: 4,
+  permissions: "MANAGE_CHANNELS",
+  bot: ["MANAGE_CHANNELS"],
+  run: async (client, message, args) => {
+    //code
+  }
+};
