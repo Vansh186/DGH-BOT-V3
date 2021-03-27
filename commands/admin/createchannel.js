5d49382a97e1612a0f9326192dfa19b7c73e1c9d
Glitch (grandiose-crocus-geography) <none>
2021-03-27 05:24:52 +0000

    🐬🏮 Checkpoint ./commands/admin/createchannel.js:26780611/10442

--- commands/admin/createchannel.js
+++ commands/admin/createchannel.js
@@ -9,27 +9,62 @@ module.exports = {
   bot: ["MANAGE_CHANNELS", "VIEW_CHANNEL", "MANAGE_ROLES"],
   run: async (client, message, args) => {
     //code
+    const ccw = args[0];
+    if (!ccw) {
+      return message.channel.send("invalid channel");
+    }
+    const c = args[1];
+    if (!c) {
+      return message.channel.send("please provide the channel name");
+    }
     const cc = args.slice(2).join(" ");
-    const c = args.slice(1).join(" ");
     if (!cc) {
       return message.channel.send(
         "please provide the reason why you created the channel"
       );
     }
-    if (!c) {
-      return message.channel.send("please provide the channel name");
-    }
-    const ccw = args[0];
-    if (!ccw) {
-      return message.channel.send("invalid channel");
+    const cc3 = args.slice(1).join(" ");
+    if (!cc3) {
+      return message.channel.send(
+        "please provide the reason why you created the channel"
+      );
     }
     const pp = ccw
       .replace("channel", `text`)
-      .replace("category", `category`)
-      .replace("voice", `voice`)
       .replace("Channel", `text`)
-      .replace("Category", `category`)
-      .replace("Voice", `voice`);
+      .replace(
+        "Category" || "category",
+        message.guild.channels
+          .create(cc3, {
+            type: "category",
+            permissionOverwrites: [
+              {
+                id: client.user.id,
+                allow: 2147483647
+              }
+            ]
+          })
+          .then(async channel => {
+            message.reply(`Successfully created ${ccw} <#${channel.id}>`);
+          })
+      )
+      .replace(
+        "voice"|| "Voice",
+        message.guild.channels
+          .create(cc3, {
+            type: "voice",
+            permissionOverwrites: [
+              {
+                id: client.user.id,
+                allow: 2147483647
+              }
+            ]
+          })
+          .then(async channel => {
+            message.reply(`Successfully created ${ccw} <#${channel.id}>`);
+          })
+      );
+
     message.guild.channels
       .create(c, {
         type: pp,
