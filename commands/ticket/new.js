3c3cd45ca1dc1bfac75ee5a5474f75002de592e4
Glitch (grandiose-crocus-geography) <none>
2021-03-27 00:21:15 +0000

    🚔☔️ Checkpoint ./commands/ticket/new.js:26780611/5420 ./server.js:26780611/884

--- commands/ticket/new.js
+++ commands/ticket/new.js
@@ -1,33 +1,28 @@
 const randomstring = require("randomstring");
 const Discord = require("discord.js");
 const db = require("quick.db");
-  let numbers = randomstring.generate({
-      length: 5,
-      charset: "numeric"
-    });
+let numbers = randomstring.generate({
+  length: 5,
+  charset: "numeric"
+});
 module.exports = {
-  name: "ticket",
-  category: "ticket",
+  name: "name",
+  usage: `usage`,
+  category: "category",
+  description: "",
+  args: false,
+  cooldown: 0,
+  permission: "",
+  run: async (client, message, args) => {
+    message.guild.channels
+      .create(`Ticket_${numbers}`, {
+        type: "text",
+        topic: "hi"
+      })
+      .then(async channel => {
+        message.reply(`click <#${channel.id}> to view your ticket`);
 
-  run: async (args, client, message) => {
- const ch = message.guild.channels.cache.find(ch => ch.name === message.author.id)
-        if(ch) return message.channel.send('You already have a ticket open.')
-        message.guild.channels.create(`${message.author.id}`, {
-            type : 'text',
-            parent : '756890395392081985',
-            permissionOverwrites : [
-                {
-                    id : message.guild.id,
-                    deny : ['VIEW_CHANNEL']
-                },
-                {
-                    id : message.author.id,
-                    allow : ['VIEW_CHANNEL', 'SEND_MESSAGES', 'ADD_REACTIONS', 'ATTACH_FILES']
-                }
-            ]
-        }).then(async channel=> {
-            message.reply(`click <#${channel.id}> to view your ticket`)
-            channel.send(`${message.author}, welcome to your ticket!`)
-        })
-    }
-}
+        channel.send(`${message.author}, welcome to your ticket!`);
+      });
+  }
+};
--- server.js
+++ server.js
@@ -210,6 +210,32 @@ client.on("message", async message => {
     client.logger.error(error);
   }
 });
+client.ticketTranscript = mongoose.model('transcripts', 
+    new mongoose.Schema({
+        Channel : String,
+        Content : Array
+    })
+)
+// -------------------------------------------------
+
+client.on('message', async(message) => {
+    if(message.channel.parentID !== '756890395392081985') return;
+    client.ticketTranscript.findOne({ Channel : message.channel.id }, async(err, data) => {
+        if(err) throw err;
+        if(data) {
+           console.log('there is data')
+           data.Content.push(`${message.author.tag} : ${message.content}`) 
+        } else {
+            console.log('there is no data')
+            data = new client.ticketTranscript({ Channel : message.channel.id, Content: `${message.author.tag} : ${message.content}`})
+        }
+        await data.save()
+            .catch(err =>  console.log(err))
+        console.log('data is saved ')
+    })
+
+})
+
 function xp(message) {
   const randomnumber = Math.floor(Math.random() * 10) + 15;
   db.add(`guild_${message.guild.id}_xp_${message.author.id}`, randomnumber);
