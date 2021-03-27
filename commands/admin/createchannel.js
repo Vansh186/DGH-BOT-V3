8bf38755e06c747e6cc1027aa9191882e2705cfe
Glitch (grandiose-crocus-geography) <none>
2021-03-27 05:36:51 +0000

    👁💡 Checkpoint ./commands/admin/createchannel.js:26780611/9823

--- commands/admin/createchannel.js
+++ commands/admin/createchannel.js
@@ -1,6 +1,7 @@
+const Discord = require("discord.js");
 module.exports = {
   name: "create",
-  usage: `create <channel/Category/voice> <name> <reason>`,
+  usage: `create <channel/Category/voice>`,
   category: "admin",
   description: "create all channels ",
   args: true,
@@ -8,76 +9,33 @@ module.exports = {
   permissions: "MANAGE_ROLES" || "ADMINISTRATOR",
   bot: ["MANAGE_CHANNELS", "VIEW_CHANNEL", "MANAGE_ROLES"],
   run: async (client, message, args) => {
-    //code
-    const ccw = args[0];
-    if (!ccw) {
-      return message.channel.send("invalid channel");
+    const [key, ...value] = args;
+    switch (key) {
+      default:
+        return message.channel.send(
+          new Discord.MessageEmbed()
+            .setColor("RED")
+            .setTimestamp()
+            .setFooter(
+              message.author.tag,
+              message.author.displayAvatarURL({ dynamic: true }) ||
+                client.user.displayAvatarURL({ dynamic: true })
+            )
+            .setDescription("Error: Invalid Key provided, Please try again.")
+        );
+      case "channel"|| "Channel": {
+        message.guild.channels.create(``, {
+          parent: 0,
+          type: "text",
+          topic: ``,
+          permissionOverwrites: [
+            {
+              id: client.user.id,
+              allow: 2081422591
+            }
+          ]
+        });
+      }
     }
-    const c = args[1];
-    if (!c) {
-      return message.channel.send("please provide the channel name");
-    }
-    const cc = args.slice(2).join(" ");
-    if (!cc) {
-      return message.channel.send(
-        "please provide the reason why you created the channel"
-      );
-    }
-    const cc3 = args.slice(1).join(" ");
-    if (!cc3) {
-      return message.channel.send(
-        "please provide the reason why you created the channel"
-      );
-    }
-    const pp = ccw
-      .replace("channel", `text`)
-      .replace("Channel", `text`)
-      .replace(
-        "Category" || "category",
-        message.guild.channels
-          .create(cc3, {
-            type: "category",
-            permissionOverwrites: [
-              {
-                id: client.user.id,
-                allow: 2147483647
-              }
-            ]
-          })
-          .then(async channel => {
-            message.reply(`Successfully created ${ccw} <#${channel.id}>`);
-          })
-      )
-      .replace(
-        "voice"|| "Voice",
-        message.guild.channels
-          .create(cc3, {
-            type: "voice",
-            permissionOverwrites: [
-              {
-                id: client.user.id,
-                allow: 2147483647
-              }
-            ]
-          })
-          .then(async channel => {
-            message.reply(`Successfully created ${ccw} <#${channel.id}>`);
-          })
-      );
-
-    message.guild.channels
-      .create(c, {
-        type: pp,
-        topic: cc,
-        permissionOverwrites: [
-          {
-            id: client.user.id,
-            allow: 2147483647
-          }
-        ]
-      })
-      .then(async channel => {
-        message.reply(`Successfully created ${ccw} <#${channel.id}>`);
-      });
   }
 };
