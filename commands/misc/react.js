cc8c1765bbe8f77fc47ed82daef796f4572e3298
Glitch (grandiose-crocus-geography) <none>
2021-03-28 11:39:44 +0000

    💘😂 Checkpoint ./commands/misc/react.js:26780611/9008

--- commands/misc/react.js
+++ commands/misc/react.js
@@ -8,33 +8,29 @@ module.exports = {
     "Emoji Reaction with ID Message and it must be the name of the emoji, not mentioning emoji",
   run: async (client, message, args) => {
     message.delete();
-    const reactionEmoji = message.guild.emojis.cache.find(
-      emoji => emoji.name === args[0]
-    );
-    if (!reactionEmoji) {
-      return message.channel.send(
-        "Please give the name of the emojis, no mention of the emojis"
-      );
-    }
-    if (isNaN(reactionEmoji)) {
-      return message.channel.send(
-        "Please give the name of the emojis, no mention of the emojis"
-      );
-    }
     if (isNaN(args[1])) {
       return message.channel.send(
         "Please provide the message id of the user or bot"
       );
     }
-    if (args[1] > 20) {
+    const reactionEmoji = message.guild.emojis.cache.find(
+      emoji => emoji.name === args[0]
+    );
+    if (!reactionEmoji) {
       return message.channel.send(
-        "Please provide the message id of the user or bot"
+        "Please name the emojis, don't mention the emojis and Default Emoji"
+      );
+    }
+    if (isNaN(reactionEmoji)) {
+      return message.channel.send(
+        "Please name the emojis, don't mention the emojis and Default Emoji"
       );
     }
     const m = await message.channel.messages.fetch(args[1]);
     const filter1 = (reaction, user) =>
       reaction.emoji.name === args[0] && user.id === message.author.id;
     await m.react(reactionEmoji);
+
     const collector1 = await m.createReactionCollector(filter1, {
       time: 60000
     });
