4dc03b0f08322d79f8fd40271607d0f118bca672
Glitch (grandiose-crocus-geography) <none>
2021-03-28 05:14:53 +0000

    🎂🏦 Checkpoint ./commands/misc/Serverinfo.js:26780611/4784

--- commands/misc/Serverinfo.js
+++ commands/misc/Serverinfo.js
@@ -1,22 +1,60 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 module.exports = {
-    name: 'serverinfo',
-    description: 'Displays server information & statistics!',
-   category: "misc",
+  name: "serverinfo",
+  description: "Displays server information & statistics!",
+  category: "misc",
   usage: "serverinfo",
-  aliases: ['info','stats','serverstats','guildinfo','guildstats'],
-      run: async (client, message, args) => {
-   const embed = new Discord.MessageEmbed()  
-  .setAuthor(message.guild.name, message.guild.iconURL)
-  .setColor(3447003)
-  .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
-  .addField('Member Count', `${message.guild.memberCount}`, true)
-  .addField('AFK Timeout', `${message.guild.afkTimeout / 60} minutes`, true)
-  .addField('AFK Channel', `${message.guild.afkChannelID === null ? 'No AFK Channel' : client.channels.get(message.guild.afkChannelID).name} (${message.guild.afkChannelID === null ? '' : message.guild.afkChannelID})`, true)
-  .addField('Location', message.guild.region, true)
-  .addField('Created', message.guild.createdAt.toLocaleString(), true)
-  .setTimestamp()
-  .setFooter(client.user.username, client.user.avatarURL);
+  aliases: ["stats", "serverstats", "guildinfo", "guildstats"],
+  run: async (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(message.guild.name, message.guild.iconURL())
+      .setThumbnail(
+          message.guild.icon
+            ? message.guild.iconURL()
+            : `https://dummyimage.com/128/7289DA/FFFFFF/&text=${encodeURIComponent(
+                message.guild.nameAcronym
+              )}`
+        ).setColor(3447003)
+      .setDescription(
+        `Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`
+      )
+      .addField(
+        "Common Information",`
+       Member Count: \`${message.guild.memberCount}\`
+       Emojis: \`message.guild.emojis.cache.size\`\n
+       Channel Categories: 
+    message.guild.channels.cache.filter(
+          channel => channel.type === "category"
+        ).size,
+        true
+      )
+        "Text Channels",
+        message.guild.channels.cache.filter(channel => channel.type === "text")
+          .size,
+        true
+      )
+        "Voice Channels",
+        message.guild.channels.cache.filter(channel => channel.type === "voice")
+          .size,
+        true
+      )
+      "AFK Timeout", `${message.guild.afkTimeout / 60} minutes`, true)
+      "AFK Channel",
+        `${
+          message.guild.afkChannelID === null
+            ? "No AFK Channel"
+            : client.channels.get(message.guild.afkChannelID).name
+        } (${
+          message.guild.afkChannelID === null ? "" : message.guild.afkChannelID
+        })`,
+        true
+      )
+      Location", message.guild.region, true)
+      Created", message.guild.createdAt.toLocaleString(), true)
+      .setImage(message.guild.splash ? message.guild.splashURL() : null)
+        .setTimestamp()
+      .setFooter(client.user.username, client.user.avatarURL);
 
-  message.channel.send({embed});
-}}
+    message.channel.send({ embed });
+  }
+};
