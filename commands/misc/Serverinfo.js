c2f2a37586bc1130004867517b84c16e29617d14
Glitch (grandiose-crocus-geography) <none>
2021-03-28 05:26:00 +0000

    🐢🍀 Checkpoint ./commands/misc/Serverinfo.js:26780611/12207

--- commands/misc/Serverinfo.js
+++ commands/misc/Serverinfo.js
@@ -9,50 +9,48 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
       .setAuthor(message.guild.name, message.guild.iconURL())
       .setThumbnail(
-          message.guild.icon
-            ? message.guild.iconURL()
-            : `https://dummyimage.com/128/7289DA/FFFFFF/&text=${encodeURIComponent(
-                message.guild.nameAcronym
-              )}`
-        ).setColor(3447003)
+        message.guild.icon
+          ? message.guild.iconURL()
+          : `https://dummyimage.com/128/7289DA/FFFFFF/&text=${encodeURIComponent(
+              message.guild.nameAcronym
+            )}`
+      )
+      .setColor(3447003)
       .setDescription(
         `Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`
       )
       .addField(
-        "Common Information",`
-       Member Count: \`${message.guild.memberCount}\`
-       Emojis: \`message.guild.emojis.cache.size\`\n
-       Channel Categories: 
-    message.guild.channels.cache.filter(
-          channel => channel.type === "category"
-        ).size,
+        "Common Information",
+        `
+       Member Count: \`${message.guild.memberCount}\`\nEmojis: \`${
+          message.guild.emojis.cache.size
+        }\`\nChannel Categories: \`${
+          message.guild.channels.cache.filter(
+            channel => channel.type === "category"
+          ).size
+        }\`\nText Channels: ${
+          message.guild.channels.cache.filter(
+            channel => channel.type === "text"
+          ).size
+        }\`\n
+       Voice Channels: 
+        \`${
+          message.guild.channels.cache.filter(
+            channel => channel.type === "voice"
+          ).size
+        }\`\nAFK Timeout: \`${message.guild.afkTimeout / 60}\` Minutes\n
+       AFK Channel: \`${
+         message.guild.afkChannelID === null
+           ? "No AFK Channel"
+           : client.channels.get(message.guild.afkChannelID).name
+       }\`\n
+       Location: \`${
+         message.guild.region
+       }\`Created: \`${message.guild.createdAt.toLocaleString()}\``,
         true
       )
-        "Text Channels",
-        message.guild.channels.cache.filter(channel => channel.type === "text")
-          .size,
-        true
-      )
-        "Voice Channels",
-        message.guild.channels.cache.filter(channel => channel.type === "voice")
-          .size,
-        true
-      )
-      "AFK Timeout", `${message.guild.afkTimeout / 60} minutes`, true)
-      "AFK Channel",
-        `${
-          message.guild.afkChannelID === null
-            ? "No AFK Channel"
-            : client.channels.get(message.guild.afkChannelID).name
-        } (${
-          message.guild.afkChannelID === null ? "" : message.guild.afkChannelID
-        })`,
-        true
-      )
-      Location", message.guild.region, true)
-      Created", message.guild.createdAt.toLocaleString(), true)
       .setImage(message.guild.splash ? message.guild.splashURL() : null)
-        .setTimestamp()
+      .setTimestamp()
       .setFooter(client.user.username, client.user.avatarURL);
 
     message.channel.send({ embed });
