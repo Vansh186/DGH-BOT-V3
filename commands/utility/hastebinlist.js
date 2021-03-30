1960fa9ba96c25475c5124eae876614f6b95723e
Glitch (grandiose-crocus-geograph) <none>
2021-03-30 16:15:45 +0000

    👰🐗 Checkpoint ./commands/utility/hastebinlist.js:26780611/597

--- commands/utility/hastebinlist.js
+++ commands/utility/hastebinlist.js
@@ -17,11 +17,19 @@ module.exports = {
   ],
   run: async (client, message, args) => {
     //code
+    let content = ""
     message.delete();
     let list = client.db.get(`hastebinlist_${message.author.id}`);
-    let li = ""
-    sourcebin.get(li)
-	.then(bin => console.log(`Name: ${bin.url} | Raw: ${bin.files[0].raw}`))  
+    for (let i = 0; i < list.length; i++) {
+    let user = client.users.get(list[i].ID.split('_')[1]).username
+
+    content += list[i].data}
+    
+    sourcebin.get(content).then(bin =>console.log(`Name: ${bin.url} | Raw: ${bin.files[0].raw} | ${bin.title}`
+        )
+      )
+
+}
       let embed = new Discord.MessageEmbed()
         .addField(
           `Hastebin List ${message.author.username}`,
