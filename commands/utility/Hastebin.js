538812fda98902a1b5f2fea644d7eabbe8db122c
Glitch (grandiose-crocus-geograph) <none>
2021-03-30 15:41:08 +0000

    🐚👴 Checkpoint ./commands/utility/Hastebin.js:26780611/13556

--- commands/utility/Hastebin.js
+++ commands/utility/Hastebin.js
@@ -5,17 +5,15 @@ const moment = require("moment");
 const sourcebin = require("sourcebin_js");
 module.exports = {
   name: "haste",
-  usage: `haste <code/text>`,
+  usage: ``,
   category: "utility",
-  args: true,
+  args: false,
   aliases: ["hastebin"],
   run: async (client, message, args) => {
     message.delete();
-  var filter = m => m.author.id === message.author.id;
+    var filter = m => m.author.id === message.author.id;
     message.channel
-      .send(
-        `:eight_pointed_black_star:| **Send Give the Code Name**`
-      )
+      .send(`:eight_pointed_black_star:| **Send Give the Code Name**`)
       .then(msg => {
         message.channel
           .awaitMessages(filter, {
@@ -36,59 +34,45 @@ module.exports = {
                     errors: ["time"]
                   })
                   .then(collected => {
-                    if (!collected.first().content.match(/[1-60][s,m,h,d,w]/g))
-                      return message.channel.send(
-                        "**The Bot Not Support This Time**"
-                      );
-                    duration = collected.first().content;
+                    let consten = collected.first().content;
                     collected.first().delete();
-                    msg
-                      .edit(
-                        ":eight_pointed_black_star:| **Now send The Present **"
+                    msg.delete();
+                    message.delete();
+                    sourcebin
+                      .create(
+                        [
+                          {
+                            name: "Made By " + message.author.username,
+                            content: consten,
+                            languageId: "JavaScript"
+                          }
+                        ],
+                        {
+                          title: room,
+                          description:
+                            'This code was created in "' +
+                            new Intl.DateTimeFormat("en-US").format(
+                              Date.now()
+                            ) +
+                            '"'
+                        }
                       )
-                      .then(msg => {
-                        message.channel
-                          .awaitMessages(filter, {
-                            max: 1,
-                            time: 20000,
-                            errors: ["time"]
-                          })
-                          .then(collected => {
-                            title = collected.first().content;
-                            collected.first().delete();
-                            msg.delete();
-                            message.delete();
-                            
-    sourcebin
-      .create(
-        [
-          {
-            name:
-              "Made By " + message.author.username,
-            content: Content,
-            languageId: "JavaScript"
-          }
-        ],
-        {
-          title: name,
-          description:
-            'This code was created in "' +
-            new Intl.DateTimeFormat("en-US").format(Date.now()) +
-            '"'
-        }
-      )
-      .then(src => {
-        client.db.push(`hastebinlist_${message.author.id}`, src.url);
-        let embed = new discord.MessageEmbed()
-          .setTitle(`Hastebin`)
-          .setColor("RANDOM")
-          .setDescription(
-            `Code:\`\`\`kt\n${Content}\n\`\`\`\n**(${src.url})**\nThis link has been saved to the hasebinlist`
-          );
-        message.channel.send(embed);
-      })
-      .catch(e => {
-        message.channel.send(`Error, try again later`);
+                      .then(src => {
+                        client.db.push(
+                          `hastebinlist_${message.author.id}`,
+                          src.url
+                        );
+                        let embed = new discord.MessageEmbed()
+                          .setTitle(`Hastebin | ${room}`)
+                          .setColor("RANDOM")
+                          .setDescription(
+                            `Code:\`\`\`kt\n${consten}\n\`\`\`\n**(${src.url})**\nThis link has been saved to the hasebinlist`
+                          );
+                        message.channel.send(embed);
+                      })
+                  });
+              });
+          });
       });
   }
 };
