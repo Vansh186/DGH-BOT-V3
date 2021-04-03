e9715e592436b60b65ddf04637e41c1dd528cc2b
Glitch (grandiose-crocus-geograph) <none>
2021-04-03 09:16:16 +0000

    🍘🏵 Checkpoint ./commands/misc/react.js:26780611/3418

--- commands/misc/react.js
+++ commands/misc/react.js
@@ -2,37 +2,34 @@ module.exports = {
   name: "react",
   category: "misc",
   args: true,
-  usage: "react <id msg> <emojiname>",
+  usage: "react <id msg> <emoji>",
   bot: ["ADD_REACTIONS", "MANAGE_MESSAGES", "MANAGE_EMOJIS"],
   description:
-    "Emoji Reaction with ID Message and it must be the name of the emoji, not mentioning emoji",
+    "Reaction Command Like NQN",
   run: async (client, message, args) => {
     message.delete();
     if (isNaN(args[0])) {
-      return message.channel.send(
-        "Please provide the message id of the user or bot"
-      ).then(m=>m.delete({timeout:5000}).catch(e=>{}));
+      return message.channel
+        .send("Please provide the message id of the user or bot")
+        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
     }
-      if (args[0].length > 19)
-      return message.channel.send(
-      "Too Long ID - 18 Limit"
-      ).then(m=>m.delete({timeout:5000}).catch(e=>{}));
-   const reactionEmoji = message.guild.emojis.cache.find(
-      emoji => emoji.name === args[1]
+    if (args[0].length > 19)
+      return message.channel
+        .send("Too Long ID - 18 Limit")
+        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
+    const reactionEmoji = message.guild.emojis.cache.find(
+      emoji => emoji.name === args[1].replace(`<:a`,``).replace(`<:`,``).replace(`>`,``)
     );
     if (!reactionEmoji) {
-      return message.channel.send(
-        "Please name the emojis, don't mention the emojis and Default Emoji"
-      ).then(m=>m.delete({timeout:5000}).catch(e=>{}));
+      return message.channel
+        .send(
+          "Please Give Emojis That Will Be In Reaction"
+        )
+        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
     }
-    if (isNaN(reactionEmoji)) {
-      return message.channel.send(
-        "Please name the emojis, don't mention the emojis and Default Emoji"
-      ).then(m=>m.delete({timeout:5000}).catch(e=>{}));
-    }
-    const m = await message.guild.messages.cache.fetch(args[0]);
+    const m = await message.channel.messages.fetch(args[0]);
     const filter1 = (reaction, user) =>
-      reaction.emoji.name === args[1] && user.id === message.author.id;
+      reaction.emoji.name === args[1].replace(`:`,``) && user.id === message.author.id;
     await m.react(reactionEmoji);
 
     const collector1 = await m.createReactionCollector(filter1);
