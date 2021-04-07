81bcf9fa40bbab186c08bf988653f745ebf9e7e5
Glitch (grandiose-crocus-geograph) <none>
2021-04-07 05:59:03 +0000

    🍚🌇 Checkpoint ./config.js:26780611/254 ./commands/utility/invite.js:26780611/252

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
-const Owner = `767726828311543820`
-const Support = `https://discord.gg/MKwyk4qdeb`
+const { Owner, Developer , Support , Dashboard } = require("../../config.js")
 module.exports = {
   name: "invite",
   aliases: ["invitelink"],
@@ -13,15 +12,14 @@ module.exports = {
   run: async (client, message, args, ) => {
     message.delete()
     const Invite = `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2084568575`, Owne = `<@${Owner}>`, Dev = `Legendary Emoji#1742`;
-    const dashboard = `https://bot-jsll.glitch.me/`
     const Embed = new Discord.MessageEmbed()
     .setColor("GREEN")
     .setTitle("🙏Thanks🙏")
     .addField("Invite Me", `[Click Me](${Invite})`, true)
     .addField("Support Server", `[Click Me](${Support})`, true)
-    .addField("Dashboard", `[Click Me](${dashboard})`, true)
-    .addField("Owner", Owne, true)
-    //.addField("Developer", Dev)
+    .addField("Dashboard", `[Click Me](${Dashboard})`, true)
+    .addField("Owner", `${Owner}, true)
+    .addField("Developer",`${Developer}`)
     .setTimestamp();
     
     return message.channel.send(Embed).catch(() => message.channel.send("Invite Link - " + Invite)).then(m=>m.delete({timeout:44000}).catch(e=>{}))
--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 exports.Token = "NzkwOTM3MDI0OTQxMTI5NzU5.X-H3gQ.F7q32WceiromiBXbfm4xAhBVLn8"; //Bot Token - Important
 exports.Owner = "767726828311543820"; //Bot Owner ID - Important
-exports.Developer = "740947753135243354"
+exports.Developer = "740947753135243354";//Bot Dev ID - Important
 exports.Default_Prefix = "!"; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
 exports.Support = `https://discord.gg/MKwyk4qdeb`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
-exports.Donate = ``; //https://www.outube.com/channel/UCpLctSYrYYQm58ul219kMwg`; //Please Don't Change It, If You Want To Support Me <3
 exports.Dashboard = ``;
-exports.Mongo = "mongodb://localhost/dghbot";
-exports.id = `790937024941129759`;//id bot
