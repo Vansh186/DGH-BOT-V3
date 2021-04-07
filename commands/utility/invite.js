863b89d6b4e228abe079a1bc617c6b1db5049279
Glitch (grandiose-crocus-geograph) <none>
2021-04-07 08:05:16 +0000

    🍩📉 Checkpoint ./commands/utility/invite.js:26780611/3039

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
-const { Owner, Developer , Support , Dashboard } = require("../../config.js")
+const { Owner, Developer, Support, Dashboard } = require("../../config.js");
 module.exports = {
   name: "invite",
   aliases: ["invitelink"],
@@ -8,20 +8,25 @@ module.exports = {
   description: "Give You My Invite Link, Etc!",
   usage: "Invite",
   guildOnly: false,
-  cooldown: 5 ,
-  run: async (client, message, args, ) => {
-    message.delete()
-    const Invite = `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2084568575`, Owne = `<@${Owner}>`, Dev = `Legendary Emoji#1742`;
+  cooldown: 5,
+  run: async (client, message, args) => {
+    message.delete();
+    const Invite = `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2084568575`;
+    const codeinvite = Support.replace("https://discord.gg/","");
+    const image = `http://invidget.switchblade.xyz/${codeinvite}`;
+
     const Embed = new Discord.MessageEmbed()
-    .setColor("GREEN")
-    .setTitle("🙏Thanks🙏")
-    .addField("Invite Me", `[Click Me](${Invite})`)
-    .addField("Support Server", `[Click Me](${Support})`)
-    .addField("Dashboard", `[Click Me](${Dashboard})`)
-    .addField("Owner", `<@${Owner}>`)
-    .addField("Developer", `<@${Developer}>`)
-    .setTimestamp();
-    
-    return message.channel.send(Embed).catch(() => message.channel.send("Invite Link - " + Invite)).then(m=>m.delete({timeout:44000}).catch(e=>{}))
+      .setColor("GREEN")
+      .setTitle("🙏Thanks🙏")
+      .addField("Invite Me", `[Click Me](${Invite})`)
+      .addField("Support Server", `[Click Me](${Support})`)
+      .addField("Dashboard", `[Click Me](${Dashboard})`)
+      .addField("Owner", `<@${Owner}>`)
+      .addField("Developer", `<@${Developer}>`)
+      .setImage(image);
+    return message.channel
+      .send(Embed)
+      .catch(() => message.channel.send("Invite Link - " + Invite))
+      .then(m => m.delete({ timeout: 44000 }).catch(e => {}));
   }
 };
