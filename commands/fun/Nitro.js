66a90e78ac66db62a3778ddcee064230b184fd1f
Glitch (grandiose-crocus-geograph) <none>
2021-04-10 09:43:48 +0000

    😜❄️ Checkpoint ./.glitch-assets:26780611/368 ./commands/fun/Nitro.js:26780611/626 ./LICENSE.md:26780611/1068 ./CODE_OF_CONDUCT.md:26780611/6765

--- commands/fun/Nitro.js
+++ commands/fun/Nitro.js
@@ -9,7 +9,7 @@ module.exports = {
       `htt${String.fromCharCode(8203)}ps://discord.${String.fromCharCode(
         8203
       )}gift/${client.random(24)}`
-      , { files: [path.join(__dirname, '..', '..', '..', '..', '..')] }
+      ,{ files: [{ attachment: 'https://cdn.glitch.com/1e39dd35-1883-442f-990c-4fc4d05732c9%2Fthumbnails%2Fnitro.png?1618047158850', name: "Screenshot.png" }]}
     );
   }
 };
